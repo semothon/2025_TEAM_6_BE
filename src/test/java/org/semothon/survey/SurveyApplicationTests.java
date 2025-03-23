fc80221c1a83dfcd179a4faf2f40428f977ded55
seongho5356 <kjeng7897@gmail.com>
2025-03-23 18:14:18 +0900

    chore: (#1) initial commit

---
+++ src/test/java/org/semothon/survey/SurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.semothon.survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
