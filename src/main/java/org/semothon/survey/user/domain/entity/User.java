60824f689478d6b072f1e304e93574fc4ae40f3b
seongho5356 <kjeng7897@gmail.com>
2025-03-24 02:13:47 +0900

    feat: (#4) 사용자 엔티티를 생성한다

---
+++ src/main/java/org/semothon/survey/user/domain/entity/User.java
@@ -0,0 +1,16 @@
+package org.semothon.survey.user.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+}
