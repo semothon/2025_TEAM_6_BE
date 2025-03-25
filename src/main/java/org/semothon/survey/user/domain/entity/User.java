6d7808611331bc22f0c15c82b1401e3d1ef325ed
seongho5356 <kjeng7897@gmail.com>
2025-03-25 20:23:07 +0900

    feat: (#6) 기존 엔티티를 삭제하고 엔티티를 재정의하낟

--- src/main/java/org/semothon/survey/user/domain/entity/User.java
+++ src/main/java/org/semothon/survey/user/domain/entity/User.java
@@ -3,13 +3,24 @@ package org.semothon.survey.user.domain.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.semothon.survey.user.domain.enumerate.UserRole;
 
 @Entity
 @Table(name = "tb_user")
 @Data
 @NoArgsConstructor
 public class User {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    @Column(name = "user_id", length = 50)
+    private String userId;
+
+    private String userName;
+    private String userAffiliation;
+    private String userNumber;
+    private String userEmail;
+    private String userPassword;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
 }
--- src/main/java/org/semothon/survey/user/domain/repository/UserRepository.java
+++ src/main/java/org/semothon/survey/user/domain/repository/UserRepository.java
@@ -3,6 +3,6 @@ package org.semothon.survey.user.domain.repository;
 import org.semothon.survey.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
     // 추가 메서드가 필요하면 여기에 작성
 }
