4f71f28a17a3bbfa74abb88789d01d070462d356
seongho5356 <kjeng7897@gmail.com>
2025-03-27 18:43:49 +0900

    refactor: (#10) 사용자 아이디, 역할 기반으로 조회하도록 변경한다

--- src/main/java/org/semothon/survey/user/domain/repository/UserRepository.java
+++ src/main/java/org/semothon/survey/user/domain/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package org.semothon.survey.user.domain.repository;
 
 import org.semothon.survey.user.domain.entity.User;
+import org.semothon.survey.user.domain.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> findByUserEmail(String email);
+    Optional<User> findByUserIdAndUserRole(String userId, UserRole userRole);
 }
--- src/main/java/org/semothon/survey/user/domain/service/GetUserInfoService.java
+++ src/main/java/org/semothon/survey/user/domain/service/GetUserInfoService.java
@@ -2,6 +2,7 @@ package org.semothon.survey.user.domain.service;
 
 import lombok.RequiredArgsConstructor;
 import org.semothon.survey.user.domain.entity.User;
+import org.semothon.survey.user.domain.entity.UserRole;
 import org.semothon.survey.user.domain.repository.UserRepository;
 import org.semothon.survey.user.exception.UserErrorType;
 import org.semothon.survey.user.exception.UserException;
@@ -14,8 +15,8 @@ public class GetUserInfoService {
 
     private final UserRepository userRepository;
 
-    public UserInfoResponse execute(String userId) {
-        User user = userRepository.findById(userId)
+    public UserInfoResponse execute(String userId, UserRole userRole) {
+        User user = userRepository.findByUserIdAndUserRole(userId, userRole)
                 .orElseThrow(()-> new UserException(UserErrorType.NOT_EXIST_AVAILABLE_USER));
 
         return UserInfoResponse.of(user.getUserId(), user.getUserName(), user.getUserRole());
