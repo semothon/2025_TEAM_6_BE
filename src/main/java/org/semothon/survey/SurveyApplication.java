04ff0454609f009ed1e7d2b7f9e993084f0942ef
seongho5356 <kjeng7897@gmail.com>
2025-03-28 03:58:01 +0900

    chore: (#15) 실행환경에 @EnableFeignClients 를 추가한다

--- src/main/java/org/semothon/survey/SurveyApplication.java
+++ src/main/java/org/semothon/survey/SurveyApplication.java
@@ -2,8 +2,10 @@ package org.semothon.survey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SurveyApplication {
 
 	public static void main(String[] args) {
