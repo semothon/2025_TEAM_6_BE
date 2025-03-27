c5ca031be799948928e504c78e017f2fc9f90600
seongho5356 <kjeng7897@gmail.com>
2025-03-27 18:55:50 +0900

    feat: (#10) 예외 상황을 추가한다

--- src/main/java/org/semothon/survey/user/exception/UserErrorType.java
+++ src/main/java/org/semothon/survey/user/exception/UserErrorType.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserErrorType implements ErrorType {
     NOT_EXIST_AVAILABLE_USER(HttpStatus.BAD_REQUEST, "해당 유저가 존재하지 않습니다."),
+    DUPLICATE_USER_ID(HttpStatus.BAD_REQUEST, "이미 사용 중인 아이디입니다."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 틀렸습니다."),
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 아이디입니다."),
     ;
 
     private final HttpStatus status;
