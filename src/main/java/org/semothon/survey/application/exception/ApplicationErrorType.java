d941eead376a7697b6444ae36abb567681bf5494
seongho5356 <kjeng7897@gmail.com>
2025-04-01 02:49:23 +0900

    refactor: (#25) 응답 dto에 맞게 재작성한다

--- src/main/java/org/semothon/survey/application/exception/ApplicationErrorType.java
+++ src/main/java/org/semothon/survey/application/exception/ApplicationErrorType.java
@@ -11,6 +11,7 @@ public enum ApplicationErrorType implements ErrorType {
     NOT_EXIST_AVAILABLE_APPLICATION(HttpStatus.BAD_REQUEST, "해당 대여신청이 존재하지 않습니다."),
     CANNOT_APPROVE_STATUS(HttpStatus.BAD_REQUEST, "승인할 수 없는 상태입니다."),
     CANNOT_REJECT_STATUS(HttpStatus.BAD_REQUEST, "거절할 수 없는 상태입니다."),
+
     ;
 
     private final HttpStatus status;
