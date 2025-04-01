8ae4c2e6418afcb60d4c995da43f6bb474e0edae
seongho5356 <kjeng7897@gmail.com>
2025-04-02 00:21:34 +0900

    feat: (#41) 강의실 상세정보를 조회한다

--- src/main/java/org/semothon/survey/classroom/presentation/response/RoomDetailResponse.java
+++ src/main/java/org/semothon/survey/classroom/presentation/response/RoomDetailResponse.java
@@ -1,13 +1,10 @@
 package org.semothon.survey.classroom.presentation.response;
 
-import org.semothon.survey.classroom.domain.entity.Building;
-
 public record RoomDetailResponse (
 
     RoomPreviewInfo roomPreviewInfos, // 수용인원, 강의실번호
 
-    Building classroomBuilding, // 대학관
+    String classroomBuilding, // 대학관
 
     String classroomCautions // 사용 시 주의 사항
-
 ){}
