141a8931e8f0986812499291df0e1c5bd2698211
seongho5356 <kjeng7897@gmail.com>
2025-04-02 00:08:10 +0900

    refactor: (#41) 등록 시간 엔티티 컬럼명을 수정한다

--- src/main/java/org/semothon/survey/availability/domain/repository/AvailabilityRepository.java
+++ src/main/java/org/semothon/survey/availability/domain/repository/AvailabilityRepository.java
@@ -7,5 +7,5 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface AvailabilityRepository extends JpaRepository<Availability, Long> {
-    List<Availability> findByRoomIdAndAvailabilityDate(Long roomId, LocalDate date);
+    List<Availability> findByClassroomIdAfterAndAvailabilityDate(Long classroomId, LocalDate date);
 }
