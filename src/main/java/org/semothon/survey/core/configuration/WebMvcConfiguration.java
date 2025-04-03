e1fb2041273dd350e5198144458d12a476d73274
seongho5356 <kjeng7897@gmail.com>
2025-04-04 02:12:28 +0900

    feat: (#56) cors 설정한다

---
+++ src/main/java/org/semothon/survey/core/configuration/WebMvcConfiguration.java
@@ -0,0 +1,23 @@
+package org.semothon.survey.core.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000",
+                        "https://kobaco-six.vercel.app",
+                        "http://localhost:5173",
+                        "https://itsmeweb.store")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3000);
+    }
+}
