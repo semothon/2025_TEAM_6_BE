16b0560bd49eb7003b2b72f233dec7d4f57cc8d6
seongho5356 <kjeng7897@gmail.com>
2025-04-06 00:31:06 +0900

    refactor: (#73) 확장자를 png로 변경한다

--- src/main/java/org/semothon/survey/core/util/FileConversionService.java
+++
@@ -1,7 +0,0 @@
-package org.semothon.survey.core.util;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileConversionService {
-    byte[] convertFileToPng(MultipartFile file) throws Exception;
-}
